3ee4b9a3b0c0cf4a0cb7bde55f4506d62d16caf4
aless aless <alessandro.farajallah@ynov.com>
2022-05-10 11:07:05 +0200

    Marchand - Combat - Map

--- Projet-python.py
+++ Projet-python.py
@@ -1,3 +1,18 @@
+class Attack:
+  def __init__(self,name,dmg,crit_chance) -> None:
+      self.name = name
+      self.dmg = dmg
+      self.crit_chance = crit_chance
+
+  def calculate_damage(self, entity = None):
+    from random import randint
+    R = randint(0,100)
+    if R < self.crit_chance:
+      if type(entity) == Player and self.type_adventurer == "Assassin":
+        return self.dmg *3
+      return self.dmg *2
+    return self.dmg
+
 class Entity:
   def __init__(self,name,hp,strength,defense):
     self.name = name
@@ -16,12 +31,26 @@ class Entity:
       amount -= self.defense
       self.hp -= amount 
 
-  def equip(self, id_weapon):
+  def equip(self, id_weapon):   ## Pour les armes
     if self.equipped_weapon != None:
       self.desequip(self.equipped_weapon)
-
     self.equipped_weapon = self.inventory[id_weapon]
     self.strength += self.inventory[id_weapon].strength_bonus
+
+
+  def EquipArmure(self,id_armure):   ## Pour les armures
+    if self.equipped_armure != None:
+      self.desequip(self.equipped_armure)
+    self.equipped_armure = self.inventory[id_armure]
+    self.defense += self.inventory[id_armure].defense_bonus
+  
+
+  def EquipRelique(self,weapon):  ## Pour les amulettes
+    if self.equipped_relique != None:
+      self.desequip(self.equipped_relique)
+    self.equipped_relique = self.inventory[weapon]
+    self.hp += self.inventory[weapon].hp_bonus
+
 
   def desequip(self, weapon):
     if type(weapon) == Weapon:
@@ -31,33 +60,58 @@ class Entity:
     elif type(weapon) == Amulette:
       self.hp -= weapon.hp_bonus
 
-  def EquipArmure(self,id_armure):
-    if self.equipped_armure != None:
-      self.desequip(self.equipped_armure)
-    pass
-  
-  def EquipRelique(self,weapon):
-    pass 
+
+class Merchant(Entity):
+  def __init__(self,items,name,money):
+    super().__init__(name,150,40,10)
+    self.money = money
+    self.inventory = items
+    self.inventory.append(Item("Petite potion de soin","heal",10,2)) 
+    self.inventory.append(Item("Moyenne potion de soin","heal",50,8))
+    self.inventory.append(Weapon("dague de fer",10,50,10))
+    self.inventory.append(Weapon("Arc en fer",30,20,10))
+    self.inventory.append(Weapon("Epee en fer",20,5,10))
+    self.inventory.append(Amulette("Amulette de fer",20))
+    self.inventory.append(Armor("Plastron en fer",5,10))
+    
+  def buy_item(self,player):
+    for i in range(len(self.inventory)):
+      print(i,"-",self.inventory[i].name,":",self.inventory[i].price)
+    print("pick an item")
+    choice = int(input())
+    Item = self.inventory[choice]
+    if player.money >= Item.price:
+      player.money -= Item.price
+      self.money += Item.price
+      player.inventory.append(Item)
+      self.inventory.remove(Item)
+    else:
+      print("Vous n'avez pas assez d'argent")
+    print("Voulez vous continuez d'acheter?")
+    print("y or n ? ( yes or no )")
+    choix = str(input())
+    if choix == "y":
+     self.buy_item(player)
+    else:
+      pass  
+      
 class Monster(Entity):
   def __init__(self,monster_type):
     # Monstre basique 
     if monster_type == "Sbire IOT":
       super().__init__(monster_type,10,2,2)
       self.inventory.append(Item("Petite Potion Soin","heal",10,2))
-      self.inventory.append(Weapon("Carte graphique",10,10))
+      self.inventory.append(Weapon("Carte graphique",10,10,25))
 
     if monster_type == "Mec de Pepytes":
       super().__init__(monster_type,15,3,5)
       self.inventory.append(Item("Petite Potion Soin","heal",10,2))
       self.inventory.append(Weapon("Prospectus Ynov",10,10,10))
 
-    if monster_type == "Ouin-Ouin B1":
+    if monster_type == "B1 informatique":
       super().__init__(monster_type,5,1,2)
       self.inventory.append(Item("Petite Potion Soin","heal",10,2))
       self.inventory.append(Weapon("Prospectus Ynov",10,10,10))
-
-    elif monster_type == "":
-      pass
 
     # Boss
     elif monster_type == "Sofiane":
@@ -87,7 +141,7 @@ class Monster(Entity):
 
     elif monster_type == "Janin":
       super().__init__(monster_type,170,25,30)
-      self.inventory.append(Item("Feuille d'emargement",""))
+      #self.inventory.append(Item("Feuille d'emargement",""))
 
 
 '''  def Loot(self):   # Permet de faire un loot aléatoire parmi l'inventaire du monstre
@@ -95,22 +149,22 @@ class Monster(Entity):
     return self.inventory[randint(0,len(self.inventory))]
 '''
 
-class Player(Entity):
+class Player(Entity):  
   def __init__(self,name,type_adventurer):
     self.type_adenturer = type_adventurer
-    if type_adventurer == "Guerrier":
+    if type_adventurer == "Guerrier":     ## bcp de vie , peu d'attack
       super().__init__(name,100,10,20)
       self.inventory.append(Weapon("Sword",10,5,1))
       self.inventory.append(Item("Potion de soin","heal",50,5))
-    elif type_adventurer == "Assassin":     # faire 2,5x les crits 
+    elif type_adventurer == "Assassin":     # faire 3x les crits | moyen vie bcp d'attaque 
       super().__init__(name,50,20,15)
-      self.inventory.append(Weapon("Dagger",5,40,1))    
+      self.inventory.append(Weapon("Dagger",5,30,1))    
       self.inventory.append(Item("Potion de soin","heal",50,5))
-    elif type_adventurer == "Archer":
+    elif type_adventurer == "Archer":       #  son arme a bcp d'attaque
       super().__init__(name,40,20,15)
       self.inventory.append(Weapon("Arc",20,20,1))
       self.inventory.append(Item("Potion de soin","heal",50,5))
-
+ 
   def open_inventory(self):
     for i in range(len(self.inventory)):
       print(i,":",self.inventory[i].name)
@@ -119,6 +173,11 @@ class Player(Entity):
     item = self.inventory[choice]
     if type(item) == Weapon:
       self.equip(choice)
+
+    elif type(item) == Armor:
+      self.EquipArmure(choice)
+    elif type(item) == Amulette:
+      self.EquipRelique(choice)
     elif type(item) == Item:
       item.use(self)
 
@@ -167,28 +226,89 @@ class Amulette(Item):
     super().use(target)
     target.defense += self.defense
 
-def Map() :
-    import msvcrt
-    Map = [
-       [0,1,2,1,0,3],
-       [0,6,2,1,0,3],
-       [0,1,2,1,4,3],
-       [0,0,2,1,0,3],
-       [2,1,2,1,5,3],
-       [0,1,2,1,0,3]
-    ] 
+#region map
+class Map():
+  def generate_map(self) :
+    # 0 = Wall, 1 = Random Fight, 2 = Danger Boss, 3 = Boss, 4 = Chest,
+    # 5 = Start, 6 = Stairs, 7 = Merchant, 8 = Door, 9 = Goal
+    #Map = 27 x 27
+    map = [
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],#1
+      [0,5,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],#2
+      [0,0,0,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,0],#3
+      [0,1,1,2,2,3,2,2,1,1,1,1,1,1,4,1,1,1,1,1,1,1,4,1,1,1,0],#4
+      [0,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,1,1,0],#5
+      [0,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,1,1,0],#6
+      [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,3,2,2,1,1,0],#7
+      [0,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,1,1,0],#8
+      [0,1,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,1,1,0],#9
+      [0,1,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],#10
+      [0,1,1,1,2,2,3,2,2,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],#11
+      [0,1,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,0],#12
+      [0,1,1,1,2,2,2,2,2,1,1,1,1,7,1,1,1,1,1,1,2,2,2,2,2,1,0],#13
+      [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,1,0],#14
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0],#15   Stair 1
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],#16
+      [0,0,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],#17   Stair 2
+      [0,1,1,1,1,7,1,1,1,1,1,1,8,8,1,1,1,1,0,1,1,1,1,1,1,1,0],#18
+      [0,1,4,1,1,1,1,1,1,1,1,1,8,8,1,1,1,1,0,1,1,1,1,1,1,1,0],#19
+      [0,1,1,1,1,1,1,1,1,1,1,1,8,8,1,1,2,2,0,1,1,1,1,1,1,1,0],#20
+      [0,1,1,1,2,2,2,2,2,1,1,1,8,8,1,1,2,2,0,1,1,1,1,1,1,1,0],#21
+      [0,1,1,1,2,2,2,2,2,1,1,1,8,8,1,4,2,2,3,1,1,1,9,1,1,1,0],#22
+      [0,1,1,1,2,2,3,2,2,1,1,1,8,8,1,1,2,2,0,1,1,1,1,1,1,1,0],#23
+      [0,1,1,1,2,2,2,2,2,1,4,1,8,8,1,1,2,2,0,1,1,1,1,1,1,1,0],#24
+      [0,1,1,1,2,2,2,2,2,1,1,1,8,8,1,1,1,1,0,1,1,1,1,1,1,1,0],#25
+      [0,1,1,1,1,1,1,1,1,1,1,1,8,8,1,1,1,1,1,1,1,1,1,1,1,1,0],#26
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#27
+    ]
     PosX = 1
     PosY = 1
-    print(Map[PosX][PosY])
+    print("You are at the position :",PosX,";",PosY)
+
+  def moove_map(self):
+    import msvcrt
+    self.action_map(PosX,PosY)
     choice_direction = msvcrt.getch()
     if choice_direction == b"d": #Right
-        PosY+=1
-    if choice_direction == b"q": #Left
-        PosY-=1
-    if choice_direction == b"z": #Up
+      if self.wall_map(PosX,PosY) == False:
         PosX+=1
-    if choice_direction == b"s": #Down
+    elif choice_direction == b"q": #Left
+      if self.wall_map(PosX,PosY) == False:
         PosX-=1
-    print(Map[PosX][PosY])
+    elif choice_direction == b"z": #Up
+      if self.wall_map(PosX,PosY) == False:
+        PosY+=1
+    elif choice_direction == b"s": #Down
+      if self.wall_map(PosX,PosY) == False:
+        PosY-=1
 
-Map()
+  def action_map(self,X,Y):
+    if self.map[X][Y] == 1:
+      #Generer un mob random
+      print("You walk peacefully")
+    if self.map[X][Y] == 2:
+      print("You feel something near you")
+    if self.map[X][Y] == 3:
+      print("You meet a boss")
+    if self.map[X][Y] == 4:
+      print("You find a chest")
+    if self.map[X][Y] == 5:
+      print("Here is the start of your story")
+    if self.map[X][Y] == 6:
+      print("You find stairs")
+    if self.map[X][Y] == 7:
+      print("You encounter a merchant")
+    if self.map[X][Y] == 8:
+      print()
+    if self.map[X][Y] == 9:
+      print("You finally locate Janin")
+
+  def wall_map(self,X,Y):
+    if self.map[X][Y] == 0:
+      print("A wall is blocking the road")
+      return True
+    else:
+      return False
+
+#endregion
+
